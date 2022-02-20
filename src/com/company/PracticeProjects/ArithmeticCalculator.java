dab2526ec37770bc937ac92292c8c81e8ac0d1c0
VarshitAnsarie21 <varshitansarie21@gmail.com>
2022-02-20 18:19:47 +0530

    comments in arithmetic calculator

--- src/com/company/PracticeProjects/ArithmeticCalculator.java
+++ src/com/company/PracticeProjects/ArithmeticCalculator.java
@@ -4,13 +4,19 @@ import java.util.Scanner;
 
 public class ArithmeticCalculator {
     public static void main(String args[]){
+
+        //declaring the two numbers and storing the result in the third variable i.e. operation output
         double num1, num2, operationOutput;
+
+        //declaring the variable for choice of operation which is entered by user
         int choice;
 
+        //declaring the scanner class object
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the numbers on which you want to perform the operation");
 
+        //taking inputs from user i.e. the two numbers
         num1 = sc.nextDouble();
         num2 = sc.nextDouble();
 
@@ -20,8 +26,10 @@ public class ArithmeticCalculator {
         System.out.println("Press 3 for 'MULTIPLICATION");
         System.out.println("Press 4 for 'DIVISION");
 
+        //taking the input from user for the choice of arithmetic operation based on above instructions
         choice = sc.nextInt();
 
+        //taking switch statement which takes the choice and performs the user opted operation
         switch (choice){
             case 1: operationOutput = num1 + num2;
                     System.out.println("The addition of two numbers is "+ num1 + " + "+num2 +" = "+ operationOutput );
